5f9d687ebd3858f904f35bde9d8b3d5b67c54c5b
Shubham Singh Sagwaliya <sss1995deva@gmail.com>
2026-07-12 22:51:56 +0530

    updated

--- Jmeter/Jenkins/execution/isolation.groovy
+++ Jmeter/Jenkins/execution/isolation.groovy
@@ -5,6 +5,8 @@ def execute() {
     def tests = engine.readTestConfiguration(params.TEST_TYPE)
     engine.prepareTestArtifacts(tests)	
     engine.executeSequential(tests)
+    engine.generateReports(tests)
+    engine.zipReports(tests)
 }
 
 return this
