45941f9f5bceb2a93599cf5d20d800a1176c1cc1
Shubham Singh Sagwaliya <sss1995deva@gmail.com>
2026-07-12 15:14:11 +0530

    updated

--- Jmeter/Jenkins/execution/smoke.groovy
+++ Jmeter/Jenkins/execution/smoke.groovy
@@ -2,15 +2,19 @@ def execute() {
 
     def engine = load 'Jmeter/Jenkins/executionEngine.groovy'
 
+    engine.prepareWorkspace()
+
     def tests = engine.readTestConfiguration(params.TEST_TYPE)
 
-	tests.each { test ->
+    tests.each { test ->
 
-    engine.runJMeter(
-        test.script,
-        "Scripts/Results/${test.name}.jtl"
-    )
-}
+        echo "Executing : ${test.name}"
+
+        engine.runJMeter(
+            test.script,
+            "Scripts/Results/${test.name}.jtl"
+        )
+    }
 }
 
 return this
